ef01c5dffa70678bd5f6604f545d08d35971cfc2
Freddy <freddy.artea@gmail.com>
2020-02-22 17:55:01 -0500

    mostrando imagenes y enlace para alter resolucion

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
        if(busqueda === '') return
 
-        const imagenesPorPagina = 10
+        const imagenesPorPagina = 30
         const key = '15357090-720e7668b286eb8bce5fa2249'
         const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`
 
