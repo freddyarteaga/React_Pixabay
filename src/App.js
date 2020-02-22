124e3042635a83e71ed58e7983deb297d864f2c4
Freddy <freddy.artea@gmail.com>
2020-02-22 16:46:33 -0500

    primeros ajustes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Formulario from './components/Formulario'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='jumbotron'>
+        <p className='lead text-center'>Buscador de Imagenes</p>
+        <Formulario />
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default App;
